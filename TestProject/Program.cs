b21f0522e6cd1634f5eaf06de8b817b6a6bf50be
Andy Wagner <Andy.Wagner@outlook.com>
2017-08-06 10:15:15 +0200

    I added "Hello World"

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -11,6 +11,7 @@ namespace TestProject
         static void Main(string[] args)
         {
             // this is the first change to be committed
+            Console.WriteLine("Hello World");
         }
     }
 }
